2895a678b2ebef391bc746ab77e160e1ab29209d
Shreyash Halge <shreyash.shalge@gmail.com>
2022-05-18 00:00:47 +0530

    Update a12.cpp

--- dsal/a12.cpp
+++ dsal/a12.cpp
@@ -1,222 +1,258 @@
-/*
-    Shreyash Halge
-    21132 (F1-Batch)
-    Date of completion: 16/05/22
+//============================================================================
+// Name        : a12_sequentialFileHandling.cpp
+// Author      : 21132 Shreyash Halge
+// Date        : 16 May 2022
+// Copyright   : Your copyright notice
+// Description : Department maintains a student information. The file contains roll number, name, division and address. Allow user to add, delete information of student. Display information of particular employee. If record of student does not exist an appropriate message is displayed. If it is, then the system displays the student details. Use sequential file to main the data.
+//============================================================================
 
-    Problem Statement:
-    Department maintains a student information. The file contains roll number, name,
-    division and address. Allow user to add, delete information of student. Display
-    information of particular employee. If record of student does not exist an appropriate
-    message is displayed. If it is, then the system displays the student details. Use
-    sequential file to main the data.
-*/
-#include <bits/stdc++.h>
+#include <iostream>
+#include<fstream>
+#include<cstring>
+#include<iomanip>
 using namespace std;
-
-class Database
+const int MAX=20;
+class Student
 {
+ int rollno;
+ char name[20],city[20];
+
 public:
-    class Student
-    {
-    private:
-        int rollNum;
-        string name;
-        string address;
-        string division;
-
-    public:
-        friend class Database;
-        Student()
-        {
-            name = "";
-            rollNum = -1;
-            division = "";
-            address = "";
-        }
-        Student(string name, int roll, string div, string addr)
-        {
-            this->name = name;
-            this->rollNum = roll;
-            this->division = div;
-            this->address = addr;
-        }
-    };
-    Student std;
-    void addData()
-    {
-        ofstream outf;
-        cout << "\nEnter name: ";
-        cin.ignore();
-        getline(cin, std.name);
-        cout << "Enter roll number: ";
-        cin >> std.rollNum;
-        cout << "Enter division: ";
-        cin >> std.division;
-        cout << "Enter address: ";
-        cin.ignore();
-        getline(cin, std.address);
-        outf.open("database", ios::app | ios::binary);
-        outf.write((char *)&std, sizeof(std)) << flush;
-        outf.close();
-        cout << "\nSuccessfully added";
-    }
-
-    void deleteData()
-    {
-        bool flag = 0;
-        int rn, x;
-        string s;
-        cout << "\nEnter roll number: ";
-        cin >> rn;
-        ifstream fin;
-        ofstream fout;
-        fout.open("newDB", ios::app | ios::binary);
-        fin.open("database", ios::in | ios::binary);
-        while (fin.read((char *)&std, sizeof(std)))
-        {
-            if (std.rollNum == rn)
-                flag = 1;
-            if (std.rollNum != rn)
-                fout.write((char *)&std, sizeof(std)) << flush;
-        }
-        remove("database");
-        rename("newDB", "database");
-        cout << (flag ? "\n Deleted roll number-" : "\nCould not find roll number-") << rn;
-    }
-
-    void searchRecord()
-    {
-        bool flag = 0;
-        int rn;
-        cout << "\nEnter roll number: ";
-        cin >> rn;
-        ifstream inf;
-        inf.open("database", ios::in | ios::binary);
-        int x;
-        while (inf.read((char *)&std, sizeof(std)))
-        {
-            if (std.rollNum == rn)
-            {
-                flag = 1;
-                cout << "\nEntry present";
-                cout << "\nName: " << std.name;
-                cout << "\nRoll number: " << std.rollNum;
-                cout << "\nDivision: " << std.division;
-                cout << "\nAddress: " << std.address;
-            }
-        }
-        if (!flag)
-            cout << "\nCould not find roll number-" << rn;
-        inf.close();
-    }
-
-} * dt;
-
-int main()
+ Student()
 {
-    bool menu = 1;
-    dt = new Database();
-    while (menu)
-    {
-        int ch;
-        cout << "\n\n1.Enter data\n2.Search data\n3.Delete data\nEnter: ";
-        cin >> ch;
-        switch (ch)
-        {
-        case 1:
-            dt->addData();
-            break;
-
-        case 2:
-            dt->searchRecord();
-            break;
-
-        case 3:
-            dt->deleteData();
-            break;
-
-        case -1:
-            menu = 0;
-            break;
-
-        default:
-            break;
-        }
-    }
-
-    return 0;
+  strcpy(name,"");
+  strcpy(city,"");
+  rollno=0;
 }
+ Student(int rollno,char name[MAX], char city[MAX])
+ {
+  strcpy(this->name,name);
+  strcpy(this->city,city);
+  this->rollno=rollno;
+ }
+ int getRollNo()
+ {
+  return rollno;
+ }
+ void displayRecord()
+ {
+
+  cout<<endl<<setw(5)<<rollno<<setw(20)<<name<<setw(5)<<setw(5)<<setw(20)<<city;
+ }
+};
+//==========File Operations ===========
+class FileOperations
+{
+ fstream file;
+public:
+ FileOperations(char* filename)
+{
+file.open(filename,ios::in|ios::out|ios::ate|ios::binary);
+}
+ void insertRecord(int rollno, char name[MAX],char city[MAX])
+ {
+  Student s1(rollno,name,city);
+  file.seekp(0,ios::end);
+  file.write((char *)&s1,sizeof(Student));
+  file.clear();
+ }
+ void displayAll()
+ {
+  Student s1;
+  file.seekg(0,ios::beg);
+  while(file.read((char *)&s1, sizeof(Student)))
+  {
+   s1.displayRecord();
+  }
+  file.clear();
+ }
+ void displayRecord(int rollNo)
+ {
+  Student s1;
+  file.seekg(0,ios::beg);
+  bool flag=false;
+  while(file.read((char*)&s1,sizeof(Student)))
+  {
+   if(s1.getRollNo()==rollNo)
+   {
+    s1.displayRecord();
+    flag=true;
+    break;
+   }
+  }
+  if(flag==false)
+  {
+   cout<<"\nRecord of "<<rollNo<<"is not present.";
+  }
+  file.clear();
+ }
+ void deleteRecord(int rollno)
+ {
+  ofstream outFile("new.dat",ios::binary);
+  file.seekg(0,ios::beg);
+  bool flag=false;
+  Student s1;
+
+  while(file.read((char *)&s1, sizeof(Student)))
+  {
+   if(s1.getRollNo()==rollno)
+   {
+    flag=true;
+    continue;
+   }
+   outFile.write((char *)&s1, sizeof(Student));
+  }
+  if(!flag)
+  {
+   cout<<"\nRecord of "<<rollno<<" is not present.";
+  }
+  file.close();
+  outFile.close();
+  remove("student.dat");
+  rename("new.dat","student.dat");
+  file.open("student.dat",ios::in|ios::out|ios::ate|ios::binary);
+ }
+ ~FileOperations()
+ {
+  file.close();
+  cout<<"\nFile Closed.";
+ }
+};
+int main() {
+ ofstream newFile("student.dat",ios::app|ios::binary);
+  newFile.close();
+  FileOperations file((char*)"student.dat");
+     int rollNo,year,choice=0;
+     char div;
+     char name[MAX],address[MAX];
+     while(choice!=5)
+     {
+         //clrscr();
+         cout<<"\n\n------Student Database-----\n";
+         cout<<"1) Add New Student\n";
+         cout<<"2) Display All Students\n";
+         cout<<"3) Search\n";
+         cout<<"4) Delete\n";
+         cout<<"5) Exit\n";
+         cout<<"Choose your choice : ";
+         cin>>choice;
+         switch(choice)
+         {
+             case 1 : //New Record
+               cout<<endl<<"Enter RollNo and name : \n";
+               cin>>rollNo>>name;
+               cout<<"Enter City : \n";
+               cin.ignore();
+               cin.getline(address,20);
+               file.insertRecord(rollNo,name,address);
+               cout<<"\nRecord Inserted.";
+               break;
+             case 2 :
+              cout<<endl<<setw(5)<<"ROLL"<<setw(20)<<"NAME"<<setw(5)<<"CITY";
+               file.displayAll();
+               break;
+             case 3 :
+               cout<<"Enter Roll number to search: ";
+               cin>>rollNo;
+                file.displayRecord(rollNo);
+
+               break;
+             case 4:
+               cout<<"Enter roll no you want to delete:";
+               cin>>rollNo;
+               file.deleteRecord(rollNo);
+               break;
+            case 5 :break;
+         }
+
+     }
+
+ return 0;
+}
+
 /*
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 1
 
-Enter name: Samarth Kamble
-Enter roll number: 21142
-Enter division: F1
-Enter address: Pune, Maharashtra
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 1
 
-Successfully added
+Enter RollNo and name : 
+21132 Shreyash
+Enter City : 
+Pune
 
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 1
+Record Inserted.
 
-Enter name: Test Student
-Enter roll number: 11111
-Enter division: F2
-Enter address: India
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 1
 
-Successfully added
+Enter RollNo and name :
+21111 Aditya
+Enter City : 
+Mumbai
 
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 2
+Record Inserted.
 
-Enter roll number: 21142
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 2
 
-Entry present
-Name: Samarth Kamble
-Roll number: 21142
-Division: F1
-Address: IndiaMaharashtra
+ ROLL                NAME CITY
+21132            Shreyash                Pune
+21111              Aditya              Mumbai
 
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 2
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 3
+Enter Roll number to search: 21132
 
-Enter roll number: 11111
+21132            Shreyash                Pune
 
-Entry present
-Name: Test Student
-Roll number: 11111
-Division: F2
-Address: India
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 4
+Enter roll no you want to delete:21111
 
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 3
 
-Enter roll number: 11111
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 2
 
- Deleted roll number-11111
+ ROLL                NAME CITY
+21132            Shreyash                Pune
 
-1.Enter data
-2.Search data
-3.Delete data
-Enter: 2
+------Student Database-----
+1) Add New Student
+2) Display All Students
+3) Search
+4) Delete
+5) Exit
+Choose your choice : 5
 
-Enter roll number: 11111
-
-Could not find roll number-11111
-
-1.Enter data
-2.Search data
-3.Delete data
-Enter: -1
+File Closed.
 */
